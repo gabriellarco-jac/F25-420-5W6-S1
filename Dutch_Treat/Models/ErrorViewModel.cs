44520c34d7bc4bbf97d85a592c4a3b955c663b6a
Gabriel Larco <gabriel.larco@johnabbott.qc.ca>
2025-09-29 10:23:01 -0400

    Add project files.

---
+++ Dutch_Treat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dutch_Treat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
