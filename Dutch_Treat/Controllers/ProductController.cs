96edb1c1c9e4ad1a4c5cdd8891170947557c905f
Gabriel Larco <gabriel.larco@johnabbott.qc.ca>
2025-10-15 10:23:32 -0400

    Base controller

--- Dutch_Treat/Controllers/ProductController.cs
+++ Dutch_Treat/Controllers/ProductController.cs
@@ -1,29 +1,32 @@
-﻿using Dutch_Treat.Data;
+﻿using Dutch_Treat.Controllers.Base;
+using Dutch_Treat.Data;
 using Dutch_Treat.Data.Interfaces;
+using DutchTreat.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dutch_Treat.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController : BaseController<Product>
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<ProductController> _logger;
         private readonly IDutchProductRepository _repository;
         private IUnitOfWork _unitOfWork;
 
-        public ProductController(ILogger<HomeController> logger, IUnitOfWork unitOfWork) //, IDutchProductRepository repository)
+        public ProductController(ILogger<ProductController> logger, IUnitOfWork unitOfWork) 
+            : base(logger, unitOfWork.ProductRepository)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
             _repository = _unitOfWork.ProductRepository;
         }
 
-        public IActionResult Index()
-        {
-            var results = _repository.GetAll()
-                .OrderBy(p => p.Category)
-                .ToList();
+        //public IActionResult Index()
+        //{
+        //    var results = _repository.GetAll()
+        //        .OrderBy(p => p.Category)
+        //        .ToList();
 
-            return View(results);
-        }
+        //    return View(results);
+        //}
     }
 }
